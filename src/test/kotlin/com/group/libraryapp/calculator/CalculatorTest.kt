8c58e594bcbad3e1f75a09245182d473f86c6d19
Kim yonggwan <sssw0101@naver.com>
2023-12-19 23:24:11 +0900

    사칙연산 계산기에 대해 테스트 코드 작성
    - Junit5를 사용하지 않고 직접 테스트 코드 작성

---
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -0,0 +1,54 @@
+package com.group.libraryapp.calculator
+
+import com.group.libraryapp.caculator.Calculator
+import java.lang.IllegalStateException
+
+fun main() {
+    val calculatorTest = CalculatorTest()
+    calculatorTest.addTest()
+    calculatorTest.minusTest()
+    calculatorTest.multiplyTest()
+}
+
+class CalculatorTest {
+
+    fun addTest() {
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.add(3)
+
+        //then
+        if (calculator.number != 8) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun minusTest() {
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.minus(3)
+
+        //then
+        if (calculator.number != 2) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun multiplyTest(){
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.add(3)
+
+        //then
+        if (calculator.number != 15) {
+            throw IllegalStateException()
+        }
+    }
+
+}

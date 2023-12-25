98530540dced4355b5745e508284c7d0f7b3ec6b
Kim yonggwan <sssw0101@naver.com>
2023-12-25 16:22:05 +0900

    Junit5 사용법과 테스트 코드 리팩토링

---
+++ src/test/kotlin/com/group/libraryapp/calculator/JunitCalculatorTest.kt
@@ -0,0 +1,70 @@
+package com.group.libraryapp.calculator
+
+import com.group.libraryapp.caculator.Calculator
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
+
+class JunitCalculatorTest {
+
+    @Test
+    fun addTest() {
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.add(3)
+
+        //then
+        assertThat(calculator.number).isEqualTo(8)
+    }
+
+    @Test
+    fun minusTest() {
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.minus(3)
+
+        //then
+        assertThat(calculator.number).isEqualTo(2)
+    }
+
+    @Test
+    fun multiplyTest() {
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.multiply(3)
+
+        //then
+        assertThat(calculator.number).isEqualTo(15)
+    }
+
+    @Test
+    fun divideTest() {
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.divide(2)
+
+        //then
+        assertThat(calculator.number).isEqualTo(2)
+    }
+
+    @Test
+    fun divideExceptionTest(){
+        //given
+        val calculator = Calculator(5)
+
+        //when && then
+        val message = assertThrows<IllegalArgumentException> {
+            calculator.divide(0)
+        }.message
+        assertThat(message).isEqualTo("0으로 나눌 수 없습니다.")
+    }
+}

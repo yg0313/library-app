3fdb4f024b7602c5d55c20103fa191f7e48f7919
Kim yonggwan <sssw0101@naver.com>
2024-01-01 22:21:44 +0900

    BookService.java를 Kotlin으로 변경하고 Optional 제거하기

--- src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.Optional
 
 interface BookRepository : JpaRepository<Book, Long> {
-    fun findByName(bookName: String): Optional<Book>
+    fun findByName(bookName: String): Book?
 }
--- src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -18,7 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class JavaBookServiceTest @Autowired constructor(
+class BookServiceTest @Autowired constructor(
     private val bookService: BookService,
     private val bookRepository: BookRepository,
     private val userRepository: UserRepository,

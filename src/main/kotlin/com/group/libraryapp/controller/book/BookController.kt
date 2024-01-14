6ac5aa057cc0b11fd84d26199b1238bfc28b3647
Kim yonggwan <sssw0101@naver.com>
2024-01-14 13:30:56 +0900

    책 통계 보여주기 - 프로덕션 코드 개발

--- src/main/kotlin/com/group/libraryapp/controller/book/BookController.kt
+++ src/main/kotlin/com/group/libraryapp/controller/book/BookController.kt
@@ -3,7 +3,9 @@ package com.group.libraryapp.controller.book
 import com.group.libraryapp.dto.book.requst.BookLoanRequest
 import com.group.libraryapp.dto.book.requst.BookRequest
 import com.group.libraryapp.dto.book.requst.BookReturnRequest
+import com.group.libraryapp.dto.book.response.BookStatResponse
 import com.group.libraryapp.service.book.BookService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -27,5 +29,18 @@ class BookController(
     @PutMapping("/book/return")
     fun returnBook(@RequestBody request: BookReturnRequest) {
         bookService.returnBook(request)
+    }
+
+    /**
+     * 대여중인 책의 권수.
+     */
+    @GetMapping("/book/loan")
+    fun countLoanedBook(): Int {
+        return bookService.countLoanedBook()
+    }
+
+    @GetMapping("/book/stat")
+    fun getBookStatistics(): List<BookStatResponse>{
+        return bookService.getBookStatistics()
     }
 }

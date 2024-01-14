1a08c36b7b9ed600e24c67e0071a8ce17382f2eb
Kim yonggwan <sssw0101@naver.com>
2024-01-14 15:22:47 +0900

    UserLoanHistoryRepository를 Querydsl으로 리팩토링 하기

--- src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -10,6 +10,7 @@ import com.group.libraryapp.dto.book.requst.BookRequest
 import com.group.libraryapp.dto.book.requst.BookReturnRequest
 import com.group.libraryapp.dto.book.response.BookStatResponse
 import com.group.libraryapp.repository.book.BookQuerydslRepository
+import com.group.libraryapp.repository.user.loanhistory.UserLoanHistoryQueryDslRepository
 import com.group.libraryapp.util.fail
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -19,7 +20,8 @@ class BookService(
     private val bookRepository: BookRepository,
     private val bookQuerydslRepository: BookQuerydslRepository,
     private val userRepository: UserRepository,
-    private val userLoanHistoryRepository: UserLoanHistoryRepository
+    private val userLoanHistoryRepository: UserLoanHistoryRepository,
+    private val userLoanHistoryQueryDslRepository: UserLoanHistoryQueryDslRepository
 ) {
 
     @Transactional
@@ -31,7 +33,7 @@ class BookService(
     @Transactional
     fun loanBook(request: BookLoanRequest){
         val book = bookRepository.findByName(request.bookName) ?: fail()
-        if(userLoanHistoryRepository.findByBookNameAndStatus(request.bookName, UserLoanStatus.LOANED) != null){
+        if(userLoanHistoryQueryDslRepository.find(request.bookName, UserLoanStatus.LOANED) != null){
             throw IllegalArgumentException("진작 대출되어 있는 책입니다")
         }
 
@@ -47,12 +49,12 @@ class BookService(
 
     @Transactional(readOnly = true)
     fun countLoanedBook(): Int {
-        return userLoanHistoryRepository.findAllByStatus(UserLoanStatus.LOANED).size
+        return userLoanHistoryQueryDslRepository.count(UserLoanStatus.LOANED).toInt()
     }
 
     @Transactional(readOnly = true)
     fun countLoanedBookByQuery(): Long {
-        return userLoanHistoryRepository.countByStatus(UserLoanStatus.LOANED)
+        return userLoanHistoryQueryDslRepository.count(UserLoanStatus.LOANED)
     }
 
     @Transactional(readOnly = true)
